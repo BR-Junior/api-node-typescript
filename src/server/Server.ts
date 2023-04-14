c8ce4f0d8c76a57dfd4f884362cb8746939d4a62
BR-Junior <bianor.tech@gmail.com>
2023-04-14 17:49:31 -0300

    feat: add tsconfig.json

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -5,14 +5,26 @@
 // @ts-ignore
 import type from 'express';
 import { Request, Response } from 'express';
+import 'dotenv/config';
+
 
 
 const server = type();
+server.use(type.json());
 
 
 server.get('/', (req:Request, res:Response) => {
-  return res.send('Hello word!');
+  return res.status(200).send('Hello word!');
 });
+
+server.post('/', (req:Request, res:Response) => {
+  return res.status(200).json(req.body);
+});
+
+server.post('/params/:id', (req:Request, res:Response) => {
+  return res.status(200).json(req.params.id);
+});
+
 
 
 export { server };
