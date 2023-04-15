e6561c393942396abd0a7b6b65f2b93d0eb15a47
BR-Junior <bianor.tech@gmail.com>
2023-04-15 00:01:34 -0300

    feat: add ICidadesDTO, ICidadesRequestDTO, ICidadesResponseDTO

---
+++ src/server/controllers/cidades/ICidadesDTO.ts
@@ -0,0 +1,17 @@
+
+export interface ICidadesDTO {
+ id?: string
+ name: string
+ name2: string
+}
+export interface ICidadesRequestDTO {
+ id?: string
+  name: string
+  name2: string
+}
+
+export interface ICidadesResponseDTO {
+ id?: string
+ name: string
+ name2: string
+}
