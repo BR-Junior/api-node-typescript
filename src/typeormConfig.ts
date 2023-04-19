9d75da265dcac55471764728d7c638d102fb598c
BR-Junior <bianor.tech@gmail.com>
2023-04-18 21:34:40 -0300

    add: typeORM

---
+++ src/typeormConfig.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+import {DataSource} from 'typeorm';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const port = process.env.DB_PORT as number | undefined;
+
+export const typeormConfig = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [`${__dirname}/**/server/models/*.{ts,js}`],
+  migrations: [`${__dirname}/**/server/models/migrations/*.{ts,js}`]
+});
