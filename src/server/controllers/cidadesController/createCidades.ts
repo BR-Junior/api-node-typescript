0de40b0a1cf88c6babfb2e7a091f60bc70dda16b
BR-Junior <bianor.tech@gmail.com>
2023-04-18 23:19:01 -0300

    add: validação de cidade que já existe

--- src/server/controllers/cidadesController/createCidades.ts
+++ src/server/controllers/cidadesController/createCidades.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 import { ICidadesRequestDTO } from './ICidadesDTO';
 import { cidadeServiceCreate } from '../../services/cidadesServices/cidadeServiceCreate';
+import {cidadesRepository} from '../../repositories/cidadesRepository/cidadesRepository';
 
 
 
@@ -16,7 +17,18 @@ export const createCidades = async (req:Request<{}, {}, ICidadesRequestDTO>, res
   // validations(req.body);
   const newCidade = req.body;
 
-  const cidade = await cidadeServiceCreate(newCidade);
+  const find = await cidadesRepository.findOneBy({name: newCidade.name});
 
-  return res.status(200).json(cidade);
+
+  if (find?.name == newCidade.name) {
+
+    return res.status(400).json('cidade já existe');
+
+  }else {
+
+    const cidade = await cidadeServiceCreate(newCidade);
+
+    return res.status(200).json(cidade);
+
+  }
 };
